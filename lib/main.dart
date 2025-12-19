6ea9b89879b7254a31b1bc82ec592839f537813c
yeonghwan.kim <yeonghwan.kim@lgepartner.com>
2025-12-19 21:03:17 +0900

    주석추가설명추가

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,6 @@ final List<Map<String, String>> users = [
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -22,7 +21,13 @@ class MyApp extends StatelessWidget {
         body: ListView.builder(
           itemCount: users.length,
           itemBuilder: (context, index) =>
+              //이런걸 함수라고 안부르고 커스텀 위젯이라고 하는구나.
+              //React로 치면 함수 컴포넌트인데 플러터에서는 커스텀 위젯이라고 함
+              //! 이거 붙인게 널값이 아니다라는걸 증명해준거임 ㅇㅇ 내가
+              //이렇게 쓰기 싫다? 그러면 ?? 이거 붙여주면됨
               UserListTitle(name: users[index]["name"]!),
+          //이런식으로
+          //   UserListTitle(name: users[index]["name"]??"이름없음"),
           // UserListTitle(name: users[index]["name"]),
         ),
         // body: ListView(
@@ -56,10 +61,32 @@ class MyApp extends StatelessWidget {
   }
 }
 
+//StatelessWidget니까 final String name 이다?? 맞음??
+//-> 문법적으로 필수는 아닌데 걍 관례임 걍 쓰셈 스테이트레스일때 걍 쓰는거임
 class UserListTitle extends StatelessWidget {
   final String name;
 
+  // UserListTitle(name: users[index]["name"]!),
+  //그럼 만약 name 이랑 age 까지 받아야한다면?? required this,name this.age
+  //이런식으로 하는건가?
   const UserListTitle({super.key, required this.name});
+  //예시 코드
+  //   class UserListTitle extends StatelessWidget {
+  //   final String name;
+  //   final int age;
+
+  //   const UserListTitle({
+  //     super.key,
+  //     required this.name,
+  //     required this.age,
+  //   });
+  // }
+
+  //근데 순서가 좀 이상해. 그럼 생성자 먼저 해주고 그러고 그 밑에서
+  //파이널 해줘야하는거 아니가?
+
+  //생성자를 먼저 할 수 없는게 -> 선언 먼저하고 그러고 생성자로 초기화 해주는거임
+  //그러고 메서드 나오는거고 ㅇㅇㅇㅇㅇㅇ
 
   @override
   Widget build(BuildContext context) {
